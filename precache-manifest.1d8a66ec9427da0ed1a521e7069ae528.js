9f9982ccef0640b4b29b0d2d96bbbbe3b7364c4e
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-03-17 21:21:26 +1100

    Updates

---
+++ precache-manifest.1d8a66ec9427da0ed1a521e7069ae528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df1ec59320bd97d4f5dda0a0ea7aa89",
+    "url": "/drag-and-drop/index.html"
+  },
+  {
+    "revision": "a53b4a7435ead6edc469",
+    "url": "/drag-and-drop/static/css/main.160f300c.chunk.css"
+  },
+  {
+    "revision": "c39af4bf8bc714406adc",
+    "url": "/drag-and-drop/static/js/2.54768bcc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/drag-and-drop/static/js/2.54768bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53b4a7435ead6edc469",
+    "url": "/drag-and-drop/static/js/main.dac5a491.chunk.js"
+  },
+  {
+    "revision": "0e7b3089497dc87ee402",
+    "url": "/drag-and-drop/static/js/runtime-main.5cef33f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3082349166c98fa1a39282eab5595cfb.js"
+  "/drag-and-drop/precache-manifest.1d8a66ec9427da0ed1a521e7069ae528.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drag-and-drop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
